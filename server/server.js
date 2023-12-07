dc7dbdb5bd1e09ea6d629c663c0484471c7a3d8e
Will Mitchell <53200009+wmitch34@users.noreply.github.com>
2023-12-07 16:21:25 -0500

    more setup from cra -> vite conversion

---
+++ server/server.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const {Server} = require('socket.io')
+const cors = require("cors");
+
+const game = require('./src/game.js');
+
+app.use(cors());
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+    cors: {
+        origin: "http://localhost:3000",
+        methods: ["GET", "POST"]
+    }
+})
+
+io.on('connection', (socket) => {
+    console.log(`User connected: ${socket.id}`);
+
+    socket.on('send_message', (data) => {
+        console.log(data, socket.id);
+        socket.broadcast.emit('recieve_message', data)
+    })
+
+    // socket.on('disconnect', () => {
+    //     console.log('A user disconnected');
+    // });
+
+})
+
+const PORT = 3001;
+
+server.listen(PORT, () => {
+    console.log(`Now listening on port ${PORT}`); 
+});
+
+app.post('/verify', (req, res) => {
+    if(game.verify(req.body)){
+        res.send(200, {response: 'winner'})
+
+    }else{
+        res.send(200, {response: 'loser'})
+    }
+});
+
+game.game();
