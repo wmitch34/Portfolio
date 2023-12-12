09df2f2baa3acfa9d848b0ee63fdb300759a2b44
Will Mitchell <53200009+wmitch34@users.noreply.github.com>
2023-12-11 20:31:58 -0500

    Added scrollbar to chatbox

--- client/src/components/ChatBox.jsx
+++ client/src/components/ChatBox.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 import "./board.css"
 import io from 'socket.io-client'
 
@@ -6,11 +6,19 @@ import io from 'socket.io-client'
 const socket = io.connect("http://localhost:3001");
 
 export default function Chatbox(props){
-
+    
+    const scrollRef = useRef(null);
     const[inputVal, setInputVal] = useState([""])
     const[chatHistory, setChathistory] = useState([])
     let user = props.user
     let setUser = props.setUser
+
+
+    useEffect(() => {
+        if (scrollRef.current) {
+            scrollRef.current.scrollTop = scrollRef.current.scrollHeight;
+        }
+    }, [chatHistory]);
 
     useEffect(()=>{
         socket.emit('req_history')
@@ -47,31 +55,45 @@ export default function Chatbox(props){
 
 
     return(
-        <>
-            <div className='w-100'>
-                <input 
-                    value={user}
-                    onChange ={(event) => {
-                        setUser(event.target.value)
-                    }}
-                    className='form-control'
-                />
+        <div className="container-fluid" >
+            <div className="row" style={{ height: '100%' }}>
+                <div className="col-12 d-flex flex-column">
+
+                    <div className="row flex-grow-1">
+                        <div>
+                            <input 
+                                value={user}
+                                onChange ={(event) => {
+                                    setUser(event.target.value)
+                                }}
+                                className='form-control'
+                            />
+
+                        </div>
+                    </div>
+             
+                    <div className="row flex-grow-3">
+                        <div className='w-100 message-window' style={{ height: '300px', overflowY: 'auto' }} ref = {scrollRef}>
+                            {chatHistory.map((message, index) =>(
+                                <p key = {index}>{message.user}: {message.message}</p>
+                            ))}
+                        </div>
+                    </div>
+
+                    <div className="row flex-grow-1">
+                        <div className='w-100 overFlow-auto '>
+                            <input 
+                                value={inputVal}
+                                onChange ={(event) =>{
+                                    setInputVal(event.target.value)
+                                }}
+                                className='form-control'
+                            />
+                            <button onClick = {handlSendMessage}>Send Message</button>
+                        </div>
+                    </div> 
+                </div>
             </div>
-            <div className='w-100'>
-                {chatHistory.map((message, index) =>(
-                    <p key = {index}>{message.user}: {message.message}</p>
-                ))}
-            </div>
-            <div className='w-100'>
-                <input 
-                    value={inputVal}
-                    onChange ={(event) =>{
-                        setInputVal(event.target.value)
-                    }}
-                    className='form-control'
-                />
-                <button onClick = {handlSendMessage}>Send Message</button>
-            </div>
-        </>
-    )
-}
+        </div>
+    );
+};
