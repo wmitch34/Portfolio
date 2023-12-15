d4b85226c34632cefaee57aa5b46a96023f82619
Will Mitchell <53200009+wmitch34@users.noreply.github.com>
2023-12-15 16:25:52 -0500

    add style to current roll in board info

--- client/src/components/BingoInfo.jsx
+++ client/src/components/BingoInfo.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import './BingoInfo.css'
 
 export default function BingoInfo(props){
     const {socket} = props
@@ -8,13 +9,14 @@ export default function BingoInfo(props){
         socket.on('rolled_number', (data)=>{
             setRoll(data)
         })
-        
     }, [socket])
 
     return(
         <>
             <h1>Info Goes here</h1>
-            <p>{roll}</p>
+            <div className='current-roll-container w-100'>
+                <div className='current-roll '>{roll}</div>
+            </div>
         </>
     )
 }
