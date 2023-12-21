d9a6d6bdf59f8081af7ad2c2a26ab04311988a42
Will Mitchell <53200009+wmitch34@users.noreply.github.com>
2023-12-21 14:31:10 -0500

    stripped alot of code

--- client/src/pages/Snake.jsx
+++ client/src/pages/Snake.jsx
@@ -1,123 +1,52 @@
-import { useState, useEffect } from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './snake.css'
+import {useState, useEffect} from 'react'
 
-export default function Snake(){
+export default function Snake(props){
 
-    function initBoard(i_input, j_input){
-        console.log("called")
-        let board = [];
-        let id = 0
+    const [position, setPosition] = useState({ x: 0, y: 0 });
     
-        for(let i = 0; i < 13; i++){
-            let row = [];
-            let state;
-            for(let j = 0; j < 13; j++){
-                if(i_input == i && j_input == j){
-                    state = 1;
-                }else state = 0;
+    useEffect(() => {
+      const handleKeyDown = (event) => {
+        const moveStep = 50; // Define how many pixels the element moves with each key press
     
-                let col = {
-                    row: i,
-                    col: j,
-                    id: id,       
-                    state: state,           
-                }
-                row.push(col)
-                id++;
-            }
-            board.push(row);        
-        }
-        console.log("Board: ",board)
-        return board
-    }
-
-    const SIZE = 13;
-    const[board, setBoard] = useState([[]])
-    const[currPosition, setCurrPosition]= useState({i:((SIZE - 1)/2), j:((SIZE - 1)/2)})
-
-
-    const handleInput = (event) =>{
-        switch(event.key){
-
-            case 'ArrowLeft':
-                console.log(event.key)
-                setCurrPosition((prev) => ({i:prev.i, j: prev.j - 1}));
-                console.log(currPosition)
-
+        // Calculate new position based on current position
+        let newPosition = { ...position };
+        switch (event.key) {
+          case 'ArrowUp':
+            newPosition.y -= moveStep;
             break;
-
-            case 'ArrowRight':
-                console.log(event.key)
-                setCurrPosition((prev) => ({i:prev.i, j: prev.j + 1})); 
-                console.log(currPosition)
-
+          case 'ArrowDown':
+            newPosition.y += moveStep;
             break;
-
-            case 'ArrowUp':
-                console.log(event.key)
-                setCurrPosition((prev) => ({i:prev.i - 1, j: prev.j}));
-                console.log(currPosition)
-
+          case 'ArrowLeft':
+            newPosition.x -= moveStep;
             break;
-
-            case 'ArrowDown':
-                console.log(event.key)
-                setCurrPosition((prev) => ({i:prev.i + 1, j: prev.j}));
-                console.log(currPosition)
-
+          case 'ArrowRight':
+            newPosition.x += moveStep;
             break;
-
-            default:
+          default:
             break;
         }
-    }
-
-    useEffect(()=>{
-        
-        setBoard(initBoard(((SIZE - 1)/2), ((SIZE - 1)/2)))        
-        
-        console.log('rerendering')
-        setTimeout(()=>{
-            window.addEventListener('keydown', handleInput);
-
-        }, 1000)
-
-    }, [])
+    
+        setPosition(newPosition);
+      };
+    
+      window.addEventListener('keydown', handleKeyDown);
+    
+      return () => {
+        window.removeEventListener('keydown', handleKeyDown);
+      };
+    }, [position]); // Listening to position could lead to infinite updates, better to listen only when mounting
     
     return (
-        <>
-                <div className='custom-cell' style={{ left: currPosition.x * 2, top: '-3rem' }}></div>
-                <div className = 'row'>
-                    <div className="col text-center">
-                        <h1 className = 'title'>Snake</h1>
-                    </div>
-                </div>
-                <div className = 'row'>
-                    <div className="col-3 align-items-center justify-content-center">
-                        <h1 className = 'title'>left</h1>
-                    </div>
-                    <div className="col-6 align-items-center justify-content-center">
-                        <h1 className = 'title'>center</h1>
-                        <div className='container'>
-                            <div className='snake-board'>
-                                {board.map((row, index) => (
-                                    <div key={index} className='snake-row w-100'>
-                                        {row.map((cell) => (
-                                            <div key={cell.id} id = {cell.id} className={'custom-cell'}>
-                                            </div>
-                                        ))}
-                                    </div>
-                                ))}
-
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col-3 align-items-center justify-content-center">
-                        <h1 className = 'title'>right</h1>
-                    </div>
-                </div>
-        </>
-    )
-
+      <div
+        style={{
+          position: 'absolute',
+          top: `${position.y}px`,
+          left: `${position.x}px`,
+          width: '50px',
+          height: '50px',
+          backgroundColor: 'blue',
+        }}
+      ></div>
+    );
 }
