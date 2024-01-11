d961cab1e03d8a88e1f8a4d69c7f045119809446
Will Mitchell <53200009+wmitch34@users.noreply.github.com>
2024-01-11 17:00:04 -0500

    Menu UX tweak

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -4,6 +4,10 @@ import { Nav, Navbar, NavDropdown } from "react-bootstrap";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 
+const menuTitle = {
+  color: "white",
+};
+
 export default function FixedNavbar(props) {
   const { btnArray } = props;
   const [navOpen, setNavOpen] = useState(false);
@@ -15,20 +19,33 @@ export default function FixedNavbar(props) {
       style={{
         zIndex: 9999999999,
         position: "fixed",
-        backgroundColor: "#121212",
+        top: 20,
+        left: 20,
       }}
     >
       <Navbar.Toggle
+        style={{ backgroundColor: "#121212" }}
         aria-controls="responsive-navbar-nav"
         onClick={() => {
           setNavOpen((prev) => !prev);
         }}
       />
-      <Navbar.Collapse id="responsive-navbar-nav" in={navOpen}>
-        <div>Select A game</div>
+      <Navbar.Collapse
+        id="responsive-navbar-nav"
+        in={navOpen}
+        style={{
+          textAlign: "center",
+          backgroundColor: "#121212",
+          padding: 5,
+          borderRadius: 20,
+          marginTop: 10,
+        }}
+      >
+        <div style={menuTitle}>Select A game</div>
         <Nav className="mr-auto">
           {btnArray.map((menuItem, index) => (
             <Nav.Link
+              key={index}
               onClick={() => {
                 menuItem.on_click();
                 setNavOpen(false);
