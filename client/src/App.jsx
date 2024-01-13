d766608be70df17560777249b13f23dd7a9cec94
Will Mitchell <53200009+wmitch34@users.noreply.github.com>
2024-01-13 15:47:49 -0500

    Adding button for type racer

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,27 +7,39 @@ import PIZZA from "../src/assets/pizza.jpg";
 export default function App() {
   const [bingo, setBingo] = useState(false);
   const [snake, setSnake] = useState(false);
+  const [typeRacer, setTypeRacer] = useState(false);
 
   let btns = [
+    {
+      text: "Home",
+      on_click: () => {
+        setBingo(false);
+        setSnake(false);
+        setTypeRacer(false);
+      },
+    },
     {
       text: "Multiplayer Bingo",
       on_click: () => {
         setBingo(true);
         setSnake(false);
+        setTypeRacer(false);
       },
     },
     {
       text: "Apple Picker",
       on_click: () => {
         setBingo(false);
+        setTypeRacer(false);
         setSnake(true);
       },
     },
     {
-      text: "Home",
+      text: "Type Racer",
       on_click: () => {
         setBingo(false);
         setSnake(false);
+        setTypeRacer(true);
       },
     },
   ];
