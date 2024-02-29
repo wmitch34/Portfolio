092daa6c9e181d19686a54a82c081d54888d1600
Will Mitchell <53200009+wmitch34@users.noreply.github.com>
2024-02-29 15:26:15 -0500

    Update main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -8,7 +8,8 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <h>Hello world</h>
+      {/* <App /> */}
     </BrowserRouter>
   </React.StrictMode>
 );
