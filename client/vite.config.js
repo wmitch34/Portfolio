0280e1404b79a7ea14c8fcdc26c3b767b1204a2d
Will Mitchell <53200009+wmitch34@users.noreply.github.com>
2024-03-01 10:33:50 -0500

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,11 +3,8 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/",
-  plugins: [react()],
-  server: {
-    port: 3000,
-  },
+  // base: "/",
+
   build: {
     assetsDir: "assets",
   },
