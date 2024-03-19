ef7fdaaf3a8327a3425b8a0bacb842537a55ff05
wmitch34 <wmitch34@gmail.com>
2024-03-18 20:45:53 -0400

    Added resume to about page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,19 +2,15 @@ import React, { useEffect, useRef } from "react";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
 import ApplePickerThumb from "../assets/applePickerThumb.png";
 import BingoThumb from "../assets/bingoThumb2.jpg";
 import TypeRacerThumb from "../assets/typeRacerThumb.png";
-import Contstruction from "../assets/construction.jpg";
 import { Link as ScrollLink, animateScroll as scroll } from "react-scroll";
-import Image from "react-bootstrap/Image";
-
 import "./Home.css";
+import Resume from "./resume.jsx";
 
 import { Link, useLocation } from "react-router-dom";
-import { alignPropType } from "react-bootstrap/esm/types";
 
 const bingoDesc =
   "Test your Bingo skills against the world's finest players over the web.";
@@ -35,7 +31,7 @@ function MyCard(props) {
     <div className="my-col hoverClass">
       <Link to={link} style={{ textDecoration: "none" }}>
         <Card>
-          <Card.Img variant="top" src={img} alt={alt} fluid />
+          <Card.Img variant="top" src={img} alt={alt} fluid="true" />
           <Card.Body>
             <Card.Title>{title}</Card.Title>
             <Card.Text
@@ -124,98 +120,24 @@ export default function Home(props) {
           </Row>
         </Container>
       </Row>
-      <Row className="lg-screen-100vh">
+      <Row>
         <Col xs={12} sm={12} md={12} lg={12} xl={12}>
           <h1 ref={myRef} id="about">
             {" "}
           </h1>
           <h1 className="text-xl mt-4">About</h1>
         </Col>
-        <Col xs={12} sm={12} md={12} lg={12} xl={12}>
-          <p>This section is currently under construction!</p>
-          <Image
-            style={{ maxHeight: "30rem" }}
-            src={Contstruction}
-            alt="Description of your image"
-            fluid
-          />
+        <Col
+          xs={12}
+          sm={12}
+          md={12}
+          lg={12}
+          xl={12}
+          className="lg-screen-100vh sm-screen-60vh"
+        >
+          <Resume />
         </Col>
       </Row>
-      {/* <Row className="mt-4">
-        <Col xs={12} sm={12} md={12} lg={12} xl={12}>
-          <Card className="mb-4">
-            <Card.Body>
-              <Card.Title>Bingo</Card.Title>
-              <Card.Subtitle className="mb-2 text-muted">
-                Multiplayer Gaming
-              </Card.Subtitle>
-              <Card.Text style={aboutStyle}>
-                Bingo started out as an intro-to-react school project. After
-                meeting the requirements for the project, I took the opotunity
-                to learn some basic CSS animations. Eventually, the gradual
-                addition of features turned bingo into an exploration of
-                animations, rest api, and web socket utlization.
-              </Card.Text>
-              <Card.Link href="/Bingo">Play now</Card.Link>
-              <Card.Link href="#">Another Link</Card.Link>
-            </Card.Body>
-          </Card>
-        </Col>
-        <Col xs={12} sm={12} md={12} lg={12} xl={12}>
-          <Card className="mb-4">
-            <Card.Body>
-              <Card.Title>Apple Picker</Card.Title>
-              <Card.Subtitle className="mb-2 text-muted">
-                Creative Outlet
-              </Card.Subtitle>
-              <Card.Text style={aboutStyle}>
-                Towards the end of my Bachelors degree, I developed an interest
-                in digital art. Some of my first attempts at drawing digital
-                characters reminded me of 2d video game assets. This inspired me
-                to create a place where I could see my art come to life! While
-                developing Apple Picker, I learned about 2d grid traversal,
-                image handling, catching user input, and much more.
-                Additionally, Apple Picker utilizes OOP concepts and 2d
-                collision calculations. One of my main take-aways from the dev
-                process was that you should not reinvent the wheel (in this
-                case, 2d game engine) if better, more rhobust tools are easily
-                available.
-              </Card.Text>
-              <Card.Link href="/ApplePicker">Start collecting</Card.Link>
-              <Card.Link href="#">Another Link</Card.Link>
-            </Card.Body>
-          </Card>
-        </Col>
-        <Col xs={12} sm={12} md={12} lg={12} xl={12}>
-          <Card className="mb-4">
-            <Card.Body>
-              <Card.Title>Type Racer</Card.Title>
-              <Card.Subtitle className="mb-2 text-muted">
-                Offline Challenge
-              </Card.Subtitle>
-              <Card.Text style={aboutStyle}>
-                In preparation for a long flight, I downloaded an offline
-                documentation tool called{" "}
-                <a href="https://devdocs.io/offline" target="_blank">
-                  DevDocs
-                </a>
-                . I spent the flight writing a words-per-minute calculator app.
-                This exercise was meant to develop my documentation-reading
-                skills and decreses reliance on frantic googling. I am really
-                happy with what I was able to accomplish in such limited time
-                with limited resources (and lots of questions from my
-                seat-neighbor). The version available here on my portfolio is
-                much the same as my original app, althouge I have optimized it
-                for mobile viewing and replaced vanilla css with Bootstrap.
-              </Card.Text>
-              <Card.Link href="/TypeRacer">Test your speed</Card.Link>
-              <Card.Link href="https://devdocs.io/offline" target="_blank">
-                Dev Docs
-              </Card.Link>
-            </Card.Body>
-          </Card>
-        </Col>
-      </Row> */}
     </Container>
   );
 }
---
+++ client/src/pages/resume.jsx
@@ -0,0 +1,231 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+
+export default function Resume() {
+  return (
+    <div
+      style={{
+        backgroundColor: "white",
+        color: "black",
+        padding: "2rem",
+      }}
+    >
+      <Container>
+        <Row>
+          <Col style={{ textAlign: "center" }}>
+            <h1>Will Mitchell</h1>
+            <p>
+              wmitch34@gmail.com |{" "}
+              <a href={"https://github.com/wmitch34"} target={"_blank"}>
+                GitHub
+              </a>{" "}
+              | (407) 790-0679
+            </p>
+          </Col>
+        </Row>
+
+        <Row>
+          <Col>
+            <h2>Profile</h2>
+            <p>
+              Experienced college graduate pursuing a software engineering
+              position where I can leverage my technical expertise and
+              interpersonal skills to build great software and contribute to a
+              great team. Seeking a challenging position where I can develop
+              creative solutions while continuously expanding my knowledge base,
+              skill set, and functional experience.
+            </p>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <h2>Skills</h2>
+            <Container>
+              <Row>
+                <Col>JavaScript / TypeScript</Col>
+                <Col>CSS</Col>
+                <Col>Java</Col>
+                <Col>UX / UI Design</Col>
+              </Row>
+              <Row>
+                <Col>React JS</Col>
+                <Col>HTML</Col>
+                <Col>SQL</Col>
+                <Col>Teaching / Instruction</Col>
+              </Row>
+            </Container>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <h2>Project Highlights</h2>
+            <p>
+              “Tanknicians” Management Portal – Web Application, Front-end
+              Developer
+            </p>
+            <ul>
+              <li>
+                Collaborated with a team of five to develop a full stack web
+                application with a cross-platform mobile app.
+              </li>
+              <li>
+                Designed a user-friendly UI with CRUD operations and data
+                visualization using React JS, Material UI, and Chart JS
+              </li>
+              <li>
+                Retrieved, formatted, and displayed JSON data from a Node JS
+                server with Express API and SQL relational database.
+              </li>
+              <li>
+                Developed, tested, and documented code using Confluence, Jira,
+                and Agile methodologies.
+              </li>
+              <li>
+                Met and beat deadlines throughout the one year timeline from
+                inception to deployment.
+              </li>
+            </ul>
+            <p>
+              “My Game List” Online Game Library – Web Application, Back-end
+              Developer
+            </p>
+            <ul>
+              <li>
+                Implemented JavaScript Express API connected to a MongoDB
+                database.
+              </li>
+              <li>
+                Designed routing system with restricted API routes using JSON
+                Web Token.
+              </li>
+              <li>
+                Collaborated with a seven-member team to implement the API into
+                a MERN stack application with mobile app.
+              </li>
+            </ul>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <h2>Experience</h2>
+            <p>
+              <Container>
+                <Row>
+                  <Col>
+                    <p>Deloitte Consulting</p>
+                  </Col>
+                  <Col>
+                    <p>Summer Solutions Scholar (Software Developer)</p>
+                  </Col>
+                  <Col>
+                    <p>June 2023 - Aug 2023</p>
+                  </Col>
+                </Row>
+              </Container>
+            </p>
+            <ul>
+              <li>
+                Participated in a 10-week internship program as a software
+                developer on a ‘Government and Public Services’ project.
+              </li>
+              <li>
+                Analyzed an Enterprise Java web application and addressed
+                documented defects with a Confluence-Jira workflow.
+              </li>
+              <li>
+                Tested software changes through front-end demos and ‘SQL
+                Developer’ database interface.
+              </li>
+              <li>
+                Collaborated with development, testing, and functional teams to
+                push code changes to a remote repository.
+              </li>
+              <li>
+                Honed interpersonal skills through ‘Communication Gyms’ and
+                ‘Diversity, Equity, and Inclusivity’ seminars for professional
+                development.
+              </li>
+            </ul>
+            <p>
+              <Container>
+                <Row>
+                  <Col>
+                    <p>University of Central Florida</p>
+                  </Col>
+                  <Col>
+                    <p>Undergraduate Learning Assistant (TA)</p>
+                  </Col>
+                  <Col>
+                    <p>Sept 2022 - Dec 2022</p>
+                  </Col>
+                </Row>
+              </Container>
+            </p>
+            <ul>
+              <li>
+                Worked with Computer Science I students to help them grasp
+                fundamental programming concepts such as data structures,
+                algorithms, and memory management using the C programming
+                language.
+              </li>
+              <li>
+                Instructed students in both one-on-one and group meetings during
+                my in-person office hours.
+              </li>
+              <li>Set and maintained my own schedule.</li>
+              <li>
+                Assisted the instructor during exam days by ensuring an ideal
+                testing environment for a full lecture hall.
+              </li>
+            </ul>
+            <p>
+              <Container>
+                <Row>
+                  <Col>
+                    <p>Marlow's Tavern</p>
+                  </Col>
+                  <Col>
+                    <p>Bartender | Trainer</p>
+                  </Col>
+                  <Col>
+                    <p>Feb 2021 - Present</p>
+                  </Col>
+                </Row>
+              </Container>
+            </p>
+            <ul>
+              <li>
+                Developed interpersonal communication skills while working with
+                a large staff and hundreds of guests.
+              </li>
+              <li>
+                Took a leadership role as the most senior certified
+                Bartender-trainer in the tavern.
+              </li>
+              <li>
+                Learned new skills by pursuing advancement opportunities such as
+                the bartending and trainer positions.
+              </li>
+              <li>
+                Observed and followed food safety guidelines to ensure the
+                health and safety of each guest that visits the tavern.
+              </li>
+            </ul>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <h2>Eeducation</h2>
+            <Container>
+              <Row>
+                <Col>Bachelor of Science, Computer Science</Col>
+                <Col>University of Central Florida</Col>
+                <Col>December 2023</Col>
+              </Row>
+            </Container>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
+}
