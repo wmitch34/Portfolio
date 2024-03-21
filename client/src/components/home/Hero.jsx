d479fb04b2a04e221afc447e60caea831e54ea1a
wmitch34 <wmitch34@gmail.com>
2024-03-21 18:42:57 -0400

    initial tailwind setup

--- client/src/components/home/Hero.jsx
+++ client/src/components/home/Hero.jsx
@@ -1,27 +1,22 @@
 import React from "react";
-import { Image, Stack, Container, Row, Col } from "react-bootstrap";
 import Portrait from "../../assets/portrait.png";
 
 export default function Hero() {
   return (
-    <Container className="">
-      <Row className="" id="image-container">
-        <Col md={6} className="flex-end">
-          <Image
-            src={Portrait}
-            alt={"Portait of Will"}
-            style={{ width: 300 }}
-          />
-        </Col>
+    <div className="">
+      <div className="" id="image-container">
+        <div className="">
+          <img src={Portrait} alt={"Portait of Will"} style={{ width: 300 }} />
+        </div>
 
-        <Col id="desc-container" md={6}>
+        <div id="desc-container">
           <h1>
             <i>WILL MITCHELL</i>
           </h1>
           <h2>Software Developer</h2>
           <h2>Computer Science, UCF 2023 </h2>
-        </Col>
-      </Row>
-    </Container>
+        </div>
+      </div>
+    </div>
   );
 }
